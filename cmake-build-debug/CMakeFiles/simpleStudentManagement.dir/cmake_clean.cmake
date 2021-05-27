ed398fe532be370cce4450bd875a47209d82e40e
David Wandratsch <david.wandratsch@htl-wels.at>
2021-05-27 12:43:32 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/simpleStudentManagement.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpleStudentManagement.dir/main.c.obj"
+  "libsimpleStudentManagement.dll.a"
+  "simpleStudentManagement.exe"
+  "simpleStudentManagement.exe.manifest"
+  "simpleStudentManagement.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simpleStudentManagement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,82 @@
+//*********************************************************************
+//
+//                  H T L - W E L S  /  Klasse 3BHIT
+//
+//---------------------------------------------------------------------
+// Programm        : simpleStudentManagement
+//
+// Ersteller       : David Wandratsch
+//
+// Erstelldatum    : 2021-05-27
+//
+// Abgabedatum     : 2021-06-01
+//
+// Kurzbeschreibung: Programm zum Erstellen einer Schülerliste und
+//                   dem Anzeigen verschiedener Informationen über ihn
+//*********************************************************************
+
+// including standard librarys
+#include <stdio.h>
+
+// defining a struct for the student
+typedef struct student
+{
+    int catNum;
+    char firstName[30];
+    char surName[30];
+    int grade;
+} sStudent;
+
+char filePath[] = "D:\\Schuljahr 2020_21\\SEW\\Exercises\\simpleStudentManagement\\";
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
+//********************************************************************
+// Funktion        : ________
+//
+// Kurzbeschreibung: ________________________________________________
+//
+// Parameter       :
+//    1   <name>_____  <typ>_____  <beschreibung>____________________
+//    2   ___________  __________  __________________________________
+//    R   -            <typ>_____  <beschreibung>____________________
+//********************************************************************
+void createFile()
+{
+
+}
+
+//********************************************************************
+// Funktion        : ________
+//
+// Kurzbeschreibung: ________________________________________________
+//
+// Parameter       :
+//    1   <name>_____  <typ>_____  <beschreibung>____________________
+//    2   ___________  __________  __________________________________
+//    R   -            <typ>_____  <beschreibung>____________________
+//********************************************************************
+void getStudentInfo()
+{
+
+}
+
+//********************************************************************
+// Funktion        : ________
+//
+// Kurzbeschreibung: ________________________________________________
+//
+// Parameter       :
+//    1   <name>_____  <typ>_____  <beschreibung>____________________
+//    2   ___________  __________  __________________________________
+//    R   -            <typ>_____  <beschreibung>____________________
+//********************************************************************
+int getAvgGrade()
+{
+
+}
